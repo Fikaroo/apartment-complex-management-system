03732d56b2fab77ad3edad8f574b697bcb7133fc
Fuad <fuadbayramv@gmail.com>
2023-04-01 22:02:31 +0400

    color design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#514EF3",
+        secondary: "#2EC4B6",
+        success: "#2DC8A8",
+        warning: "#FE8084",
+        dark: "#092C4C",
+      },
+    },
   },
   plugins: [],
 };
